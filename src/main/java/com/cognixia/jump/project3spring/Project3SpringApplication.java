103a01edb15d98adf256ca9c9c30bbc6007ac6e5
chriswoo59 <wooc99@gmail.com>
2022-11-11 13:00:43 -0500

    base spring app

---
+++ src/main/java/com/cognixia/jump/project3spring/Project3SpringApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.project3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3SpringApplication.class, args);
+	}
+
+}
