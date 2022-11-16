964b7a4a2b10671a5f54b14316cafad203cafb3c
chriswoo59 <wooc99@gmail.com>
2022-11-16 16:27:30 -0500

    Basic updateUser
    NOTE: Makes JWT unusable. Find a workaround or manually update every variable

--- src/main/java/com/cognixia/jump/project3spring/controller/UserController.java
+++ src/main/java/com/cognixia/jump/project3spring/controller/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +46,13 @@ public class UserController {
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(created);
 		
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User user) throws ResourceNotFoundException {
+		User updated = service.updateUser(id, user);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(updated);
 	}
 	
 	@DeleteMapping("/{id}")
--- src/main/java/com/cognixia/jump/project3spring/service/UserService.java
+++ src/main/java/com/cognixia/jump/project3spring/service/UserService.java
@@ -1,6 +1,7 @@
 package com.cognixia.jump.project3spring.service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +17,14 @@ public class UserService {
 
 	@Autowired
 	UserRepository repo;
-	
+
 	@Autowired
 	PasswordEncoder encoder;
 
 	public List<User> getUsers() {
 		return repo.findAll();
 	}
-	
+
 	public User getUserById(Long id) throws ResourceNotFoundException {
 		Optional<User> found = repo.findById(id);
 		if (found.isPresent()) {
@@ -41,14 +42,24 @@ public class UserService {
 		return repo.save(user);
 	}
 
+	public User updateUser(Long id, User user) throws ResourceNotFoundException {
+		Optional<User> found = repo.findById(id);
+		if (found.isPresent()) {
+			// Just in case id is different, we want this to update the same id
+			user.setId(id);
+			return repo.save(user);
+		}
+		throw new ResourceNotFoundException("User", id);
+	}
+
 	public User deleteUser(Long id) throws ResourceNotFoundException {
 		Optional<User> found = repo.findById(id);
 		if (found.isPresent()) {
-			repo.delete(found.get());;
+			repo.delete(found.get());
+			;
 			return found.get();
 		}
 		throw new ResourceNotFoundException("User", id);
 	}
 
-	
 }
