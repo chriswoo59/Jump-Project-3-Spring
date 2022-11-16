3bd076b3f4ce23d5a2d3faeecd24eb82932c3c4c
Parker Manns <114598612+pcmanns@users.noreply.github.com>
2022-11-16 09:24:01 -0600

    Add Swagger Title

--- src/main/java/com/cognixia/jump/project3spring/Project3SpringApplication.java
+++ src/main/java/com/cognixia/jump/project3spring/Project3SpringApplication.java
@@ -3,7 +3,14 @@ package com.cognixia.jump.project3spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+
+@OpenAPIDefinition(
+info = @Info( title="Restaurant API", version="1.0",
+		 description="API that allows a Restaurant to keep track of orders"))
 public class Project3SpringApplication {
 
 	public static void main(String[] args) {
