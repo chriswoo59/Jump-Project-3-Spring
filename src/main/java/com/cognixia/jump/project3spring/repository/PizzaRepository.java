5330c430785e8ca5293d9db1fb6581e7f339d22f
chriswoo59 <wooc99@gmail.com>
2022-11-16 11:40:18 -0500

    Updated Food to be one table

--- src/main/java/com/cognixia/jump/project3spring/repository/PizzaRepository.java
+++ src/main/java/com/cognixia/jump/project3spring/repository/PizzaRepository.java
@@ -1,11 +1,10 @@
 package com.cognixia.jump.project3spring.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cognixia.jump.project3spring.model.Pizza;
 
 @Repository
-public interface PizzaRepository extends JpaRepository<Pizza, Long>{
+public interface PizzaRepository extends FoodRepository<Pizza>{
 
 }
--- src/main/java/com/cognixia/jump/project3spring/service/FoodService.java
+++ src/main/java/com/cognixia/jump/project3spring/service/FoodService.java
@@ -1,5 +1,6 @@
 package com.cognixia.jump.project3spring.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,16 +12,29 @@ import com.cognixia.jump.project3spring.model.Burger;
 import com.cognixia.jump.project3spring.model.Food;
 import com.cognixia.jump.project3spring.model.Pizza;
 import com.cognixia.jump.project3spring.repository.BurgerRepository;
+import com.cognixia.jump.project3spring.repository.FoodRepository;
 import com.cognixia.jump.project3spring.repository.PizzaRepository;
 
 @Service
 public class FoodService {
+	
+	@Autowired
+	FoodRepository<Food> repo;
 
 	@Autowired
 	BurgerRepository brepo;
 	
 	@Autowired
 	PizzaRepository prepo;
+	
+	public List<Food> getAllFood() {
+		List<Food> foods = new ArrayList<Food>();
+		foods.addAll(brepo.findAll());
+		foods.addAll(prepo.findAll());
+				
+		//return foods;
+		return repo.findAll();
+	}
 	
 	public List<Burger> getAllBurgers() {
 		return brepo.findAll();
@@ -51,6 +65,15 @@ public class FoodService {
 		Optional<Pizza> found = prepo.findById(id);
 		if (found.isPresent()) {
 			prepo.delete(found.get());
+			return found.get();
+		}
+		throw new ResourceNotFoundException("Pizza", id);
+	}
+
+	public Food deleteFood(Long id) throws ResourceNotFoundException {
+		Optional<Food> found = repo.findById(id);
+		if (found.isPresent()) {
+			repo.delete(found.get());
 			return found.get();
 		}
 		throw new ResourceNotFoundException("Pizza", id);
